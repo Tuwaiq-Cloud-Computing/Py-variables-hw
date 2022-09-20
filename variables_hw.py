bc78c8550a576325d416ab4e643fcab9247d1c94
SaraMAbanmi <105993032+SaraMAbanmi@users.noreply.github.com>
2022-09-20 20:51:34 +0300

    Sara Abanmi

--- variables_hw.py
+++ variables_hw.py
@@ -8,11 +8,11 @@ print(id(str))
 print("observations: \n")
 
 # ---
-list = [1,2,3]
-print(type(list))
-print(id(list))
-list[0] = 5
-print(id(list))
+list1 = [1,2,3]
+print(type(list1))
+print(id(list1))
+list1[0] = 5
+print(id(list1))
 
 print("observations: \n")
 
@@ -29,12 +29,26 @@ print(id(my_dict))
 print("observations: \n")
 
 # --- 
-tuple = 1,2,3
-print(type(tuple))
-print(id(tuple))
+tuple1 = ('1','2','3')
+print(type(tuple1))
+print(id(tuple1))
+
+y = list (tuple1)
+y.remove('1')
+tuple1 = tuple(y)
+
+print(id(tuple1))
 
 print("observations: \n")
 
+#tuple[0] = 2
+
 # # Uncomment the below code and run it, can u explain what happened?
-# tuple[0] = 2
+#tuple[0] = 2
 #print("observations: \n")
+
+'''
+when you create  a tuple it's values can't be change unlike the others 
+but you can change it by convert it into a list change the values
+ then convert back to tuple
+'''
