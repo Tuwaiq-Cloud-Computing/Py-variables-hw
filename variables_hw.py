f7c256c0e538073f645e8175feddca1c682d4a67
Ahmed Alzahrani <113826794+ahmed-zh@users.noreply.github.com>
2022-09-21 10:31:14 +0300

    Add files via upload

--- variables_hw.py
+++ variables_hw.py
@@ -1,40 +1,40 @@
 # ---
-str = "Hello world"
-print(type(str))
-print(id(str))
-str +=  " from twuaiq!"
-print(id(str))
+# str = "Hello world"
+# print(type(str))
+# print(id(str))
+# str +=  " from twuaiq!"
+# print(id(str))
 
-print("observations: \n")
-
-# ---
-list = [1,2,3]
-print(type(list))
-print(id(list))
-list[0] = 5
-print(id(list))
-
-print("observations: \n")
-
+# print("observations: \n first print identfind data type \n 2nd the uniqe id for str ")
 
 # ---
-my_dict ={"Name":"Ahmed","Age":43}
-print(type(my_dict))
-print(id(my_dict))
-print(my_dict["Age"])
-my_dict["Age"] = 19
-print(my_dict["Age"])
-print(id(my_dict))
+# list = [1,2,3]
+# print(type(list))
+# print(id(list))
+# list[0] = 5
+# print(id(list))
 
-print("observations: \n")
+# print("observations: \nfirst print identfind data type for list \n 2nd the uniqe id for str")
 
-# --- 
+
+# # ---
+# my_dict ={"Name":"Ahmed","Age":43}
+# print(type(my_dict))
+# print(id(my_dict))
+# print(my_dict["Age"])
+# my_dict["Age"] = 19
+# print(my_dict["Age"])
+# print(id(my_dict))
+
+# print("observations: \nprint data type for my_dict \n 2nd print the specific id for my_dict\n print age")
+
+# # --- 
 tuple = 1,2,3
 print(type(tuple))
 print(id(tuple))
 
-print("observations: \n")
+# print("observations: \nprint data type for tuple \n print uniqe id for tuple")
 
 # # Uncomment the below code and run it, can u explain what happened?
-# tuple[0] = 2
-#print("observations: \n")
+tuple[0] = 2
+print("observations: \nhave no index")
