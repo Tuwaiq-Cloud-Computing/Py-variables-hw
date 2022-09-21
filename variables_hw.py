522527a76a91c673676d3a1b77fe265343639955
ReemaSalemAlghamdi <111075303+ReemaSalemAlghamdi@users.noreply.github.com>
2022-09-21 04:05:02 +0300

    Add files via upload

--- variables_hw.py
+++ variables_hw.py
@@ -6,16 +6,18 @@ str +=  " from twuaiq!"
 print(id(str))
 
 print("observations: \n")
+print("the function (id) changes when the value of the string variable (str) changes \n")
 
 # ---
 list = [1,2,3]
 print(type(list))
 print(id(list))
 list[0] = 5
+print(list)
 print(id(list))
 
 print("observations: \n")
-
+print("The id for a variable of type (list) remains the same even when one of the values is changed \n")
 
 # ---
 my_dict ={"Name":"Ahmed","Age":43}
@@ -27,6 +29,7 @@ print(my_dict["Age"])
 print(id(my_dict))
 
 print("observations: \n")
+print("The id for a variable of type (dictionary) remains the same even when one of the values is changed \n")
 
 # --- 
 tuple = 1,2,3
@@ -35,6 +38,9 @@ print(id(tuple))
 
 print("observations: \n")
 
-# # Uncomment the below code and run it, can u explain what happened?
-# tuple[0] = 2
-#print("observations: \n")
+print("the values of the variable (tuple) are Immutable, so no change can be made \n")
+
+tuple [0] = 2
+print("observations: \n")
+
+
